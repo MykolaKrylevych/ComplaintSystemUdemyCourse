9b35cd3ebdd2c15f6764cf076a2eea73c7ac2324
Mykola Krylevych <krilevickola@gmail.com>
2024-07-20 17:43:05 +0200

    Format files and add CORS

--- managers/auth.py
+++ managers/auth.py
@@ -15,7 +15,7 @@ class AuthManager:
         try:
             payload = {
                 "sub": user_data["id"],
-                "exp": datetime.utcnow() + timedelta(minutes=120)
+                "exp": datetime.utcnow() + timedelta(minutes=120),
             }
             return jwt.encode(payload, config("SECRET_KEY"), algorithm="HS256")
         except Exception as ex:
@@ -24,13 +24,19 @@ class AuthManager:
 
 
 class CustomHTTPBearer(HTTPBearer):
-    async def __call__(self, request: Request) -> Optional[HTTPAuthorizationCredentials]:
+    async def __call__(
+        self, request: Request
+    ) -> Optional[HTTPAuthorizationCredentials]:
         # check if token valid
         res = await super().__call__(request)
         # processing jwt
         try:
-            payload = jwt.decode(res.credentials, config("SECRET_KEY"), algorithms=["HS256"])
-            user_data = await database.fetch_one(user.select().where(user.c.id == payload["sub"]))
+            payload = jwt.decode(
+                res.credentials, config("SECRET_KEY"), algorithms=["HS256"]
+            )
+            user_data = await database.fetch_one(
+                user.select().where(user.c.id == payload["sub"])
+            )
             request.state.user = user_data
             return user_data
         except jwt.ExpiredSignatureError:
--- resources/complaint.py
+++ resources/complaint.py
@@ -13,24 +13,36 @@ async def get_complaints(request: Request):
     return await ComplaintManager.get_complaints(user)
 
 
-@router.post("/complaints/", dependencies=[Depends(oauth2_scheme), Depends(is_complainer)])
+@router.post(
+    "/complaints/", dependencies=[Depends(oauth2_scheme), Depends(is_complainer)]
+)
 async def create_complaint(request: Request, complaint: ComplaintIn):
     user = request.state.user
     return await ComplaintManager.create_complaint(complaint.dict(), user)
 
 
-@router.delete("/complaints/{complaint_id}/", dependencies=[Depends(oauth2_scheme), Depends(is_admin)], status_code=204)
+@router.delete(
+    "/complaints/{complaint_id}/",
+    dependencies=[Depends(oauth2_scheme), Depends(is_admin)],
+    status_code=204,
+)
 async def delete_complaint(complaint_id: int):
     await ComplaintManager.delete(complaint_id=complaint_id)
 
 
-@router.put("/complaints/{complaint_id}/approve", dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
-            status_code=204)
+@router.put(
+    "/complaints/{complaint_id}/approve",
+    dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
+    status_code=204,
+)
 async def approve_complaint(complaint_id: int):
     await ComplaintManager.approve(complaint_id)
 
 
-@router.put("/complaints/{complaint_id}/reject", dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
-            status_code=204)
+@router.put(
+    "/complaints/{complaint_id}/reject",
+    dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
+    status_code=204,
+)
 async def approve_complaint(complaint_id: int):
     await ComplaintManager.reject(complaint_id)
