1c74112da46441be259d7c1ee86b2b24109aff18
Mykola Krylevych <krilevickola@gmail.com>
2024-07-20 03:59:48 +0200

    Base structure of the complait system

---
+++ resources/user.py
@@ -0,0 +1,26 @@
+from typing import Optional, List
+
+from fastapi import APIRouter, Depends
+from managers.user import UserManager
+from managers.auth import oauth2_scheme, is_admin
+from schemas.response.user import UserOut
+from models import RoleType
+
+router = APIRouter(tags=["Users"])
+
+
+@router.get("/users/", dependencies=[Depends(oauth2_scheme), Depends(is_admin)], response_model=List[UserOut])
+async def get_all_users(email: Optional[str] = None):
+    if email:
+        return await UserManager.get_by_email(email)
+    return await UserManager.get_all_users()
+
+
+@router.put("/users/{user_id}/make_admin", dependencies=[Depends(oauth2_scheme), Depends(is_admin)], status_code=204)
+async def make_admin(user_id: int):
+    await UserManager.change_role(RoleType.admin, user_id)
+
+
+@router.put("/users/{user_id}/make_approver", dependencies=[Depends(oauth2_scheme), Depends(is_admin)], status_code=204)
+async def make_admin(user_id: int):
+    await UserManager.change_role(RoleType.approver, user_id)
